19895969045287661f661db621d5f9b5c0722720
Darnel Castor <44904668+Dcastor21@users.noreply.github.com>
2024-06-28 23:53:49 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$db_user="u606189373_Reg";
-$db_pass="DCLaker21";
-$db_name="u606189373_Login";
-$db= new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+$db_user="if0_36727139";
+$db_pass="GlHOTBHHSh3G";
+$db_name="if0_36727139_register";
+$db= new PDO('mysql:host=sql107.infinityfree.com;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
